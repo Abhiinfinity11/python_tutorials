eacc4525e3fedb9eaa6249dcd8e0342acb2fd351
Abhiinfinity11 <136891826+Abhiinfinity11@users.noreply.github.com>
2023-06-24 12:22:54 +0530

    Create number-character.py

---
+++ number-character.py
@@ -0,0 +1,25 @@
+
+#whatever is present in ' ' will be characters
+print('a')
+print('b')
+print('a')
+print('123')
+print('11p')
+print('4jdk')
+print('9 + 886686868666868686868686868')
+a = 100000000000000000000
+b = 80
+print(a + b + a + b + a)
+print(a +  b)
+print(a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a+a + b + a + b + a) 
+
+print("Keith is the man !!!!")
+print(4**2) 
+
+name = 'Abhi'
+print(name)
+day = 'vardhan'
+age = 11
+print(age)
+print(name + ' is ' + str(age) + (name) + ' years old')
+print(name + day )
